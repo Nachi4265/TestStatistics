2a674dc1bff8013b54dcafa87a0db882b33546f0
Nachi4265 <hvirgen@my.yearupunited.org>
2025-10-03 12:43:48 -0400

    finished

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,7 +1,30 @@
 package com.pluralsight;
 
+import javax.crypto.spec.PSource;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //Array of test scores
+        int[]testScores = {45,50,60,70,80,95,99};
+
+        int highest= 0;
+        int lowest = 100;
+        int sum = 0;
+
+
+        for( int testScore : testScores ){
+            sum += testScore;
+            highest = Math.max(highest,testScore);
+            lowest = Math.min(lowest,testScore);
+        }
+        float average = (float) sum / testScores.length;
+        System.out.println(average);
+        System.out.println(lowest);
+        System.out.println(highest);
+        }
+
+
+
+
     }
-}
